aa23bd05cd5572b57a1d3a7a4f2a835089297b97
romaindblr <81305762+romaindblr@users.noreply.github.com>
2021-09-26 16:41:34 +0200

    Update exercice_13_14.py

--- exercice_13_14.py
+++ exercice_13_14.py
@@ -8,12 +8,12 @@ class mention:
         self.note = float(input("Quelle est ta note : "))
         self.Note(self.note, 0, 10, "None")
         self.Note(self.note, 10, 12, "P")
-        self.Note(self.note, 12, 15, "AB")
-        self.Note(self.note, 15, 17, "B")
-        self.Note(self.note, 17, 20, "TB")
+        self.Note(self.note, 12, 14, "AB")
+        self.Note(self.note, 14, 16, "B")
+        self.Note(self.note, 16, 20, "TB")
 
     def Note(self, note, note_min, note_max, mention):
-        if float(note) > int(note_min) and note <= int(note_max):
+        if float(note) >= int(note_min) and note < int(note_max):
             print("Ta mention est :", mention)
             exit()
         else:
